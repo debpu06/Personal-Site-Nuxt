1978d9f3e61c10d91b6898d7cef3afdc00417d33
David Boland <david.boland@makingwaves.com>
2018-08-31 17:45:01 -0500

    Added environment variables for build server

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,5 @@
-const config = require('./.contentful.json')
 const contentful = require('contentful')
+const config = require('./.contentful.json')
 //const axios = require('axios')
 
 module.exports = {
@@ -88,10 +88,10 @@ module.exports = {
     }
   },
   env: {
-    CTF_SPACE_ID: config.CTF_SPACE_ID,
-    CTF_CDA_ACCESS_TOKEN: config.CTF_CDA_ACCESS_TOKEN,
-    CTF_PERSON_ID: config.CTF_PERSON_ID,
-    CTF_BLOG_POST_TYPE_ID: config.CTF_BLOG_POST_TYPE_ID
+    CTF_SPACE_ID: process.env.CONTENTFUL_SPACE_ID || config.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: process.env.CTF_CDA_ACCESS_TOKEN || config.CTF_CDA_ACCESS_TOKEN,
+    CTF_PERSON_ID: process.env.CTF_PERSON_ID || config.CTF_PERSON_ID,
+    CTF_BLOG_POST_TYPE_ID: process.env.CTF_BLOG_POST_TYPE_ID || config.CTF_BLOG_POST_TYPE_ID
   },
 }
 
