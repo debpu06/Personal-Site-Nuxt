98bdfb13ca0001d5444f235407901adcb1314857
David Boland <david.boland@makingwaves.com>
2018-09-08 13:32:53 -0500

    Added custom styles to the navbar to adjust for mobile

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -4,6 +4,13 @@
     <nav class="navbar is-link" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
         <a class="navbar-item" href="/">Home</a>
+        <a class="navbar-item is-hidden-desktop is-hidden-widescreen is-hidden-fullhd" href="/about/">About Me</a>
+        <a class="navbar-item bd-is-rss is-small is-hidden-desktop is-hidden-widescreen is-hidden-fullhd" href="/rss.xml">
+            <span class="icon is-small">
+              <i class="fa fa-rss"></i>
+            </span>
+            <span>Subscribe</span>
+          </a>
       </div>
       <div class="navbar-menu" id="navbarMenu">
         <div class="navbar-end">
